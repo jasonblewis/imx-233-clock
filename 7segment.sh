723b6f0d87772491b41984ec15a40af933892f5e
root <root@clock.lan>
2013-12-31 00:17:38 +1100

    can't quite write numbers yet but getting there

--- 7segment.sh
+++ 7segment.sh
@@ -13,6 +13,19 @@ segments=( ["sega"]="16"
     ["segg"]="2"
     ["segdp"]="1")
 
+# mapping of segments to numbers
+#   a b c d e f g dp
+declare -A d0=(["sega"]="0" ["segb"]="0" ["segc"]="0" ["segd"]="0" ["sege"]="0" ["segf"]="0" ["segg"]="0" ["segdp"]="0" )
+declare -A d1=(["sega"]="0" ["segb"]="1" ["segc"]="1" ["segd"]="0" ["sege"]="0" ["segf"]="0" ["segg"]="0" ["segdp"]="0" )
+declare -A d2=(["sega"]="1" ["segb"]="1" ["segc"]="0" ["segd"]="1" ["sege"]="1" ["segf"]="0" ["segg"]="1" ["segdp"]="0" )
+declare -A d3=(["sega"]="1" ["segb"]="1" ["segc"]="1" ["segd"]="1" ["sege"]="0" ["segf"]="0" ["segg"]="1" ["segdp"]="0" )
+declare -A d4=(["sega"]="0" ["segb"]="1" ["segc"]="1" ["segd"]="0" ["sege"]="0" ["segf"]="1" ["segg"]="1" ["segdp"]="0" )
+declare -A d5=(["sega"]="1" ["segb"]="0" ["segc"]="1" ["segd"]="1" ["sege"]="0" ["segf"]="1" ["segg"]="1" ["segdp"]="0" )
+declare -A d6=(["sega"]="1" ["segb"]="0" ["segc"]="1" ["segd"]="1" ["sege"]="1" ["segf"]="1" ["segg"]="1" ["segdp"]="0" )
+declare -A d7=(["sega"]="1" ["segb"]="1" ["segc"]="1" ["segd"]="0" ["sege"]="0" ["segf"]="0" ["segg"]="0" ["segdp"]="0" )
+declare -A d8=(["sega"]="1" ["segb"]="1" ["segc"]="1" ["segd"]="0" ["sege"]="0" ["segf"]="1" ["segg"]="1" ["segdp"]="0" )
+declare -A d9=(["sega"]="1" ["segb"]="1" ["segc"]="1" ["segd"]="1" ["sege"]="1" ["segf"]="1" ["segg"]="0" ["segdp"]="0" )
+
 function all_off {
     for i in "${segments[@]}"
     do 
@@ -39,6 +52,15 @@ function toggle_segment {
     echo $(( $(cat $gpio"${segments[seg$1]}"/value) ^ 1 )) > $gpio"${segments[seg$1]}"/value
 }
 
+function display_number {
+    all_off
+    echo showing number $1
+    for i in ${!segments[@]}
+    do
+        echo segment $i is ${ ${d[$1]}[$i] }
+    done
+}
+
 # initialise 
 #for i in 1 2 4 5 6 7 16 51
 for i in "${segments[@]}"
@@ -62,6 +84,10 @@ do
             ;;
         a | b | c | d | e | f | g | dp)
             toggle_segment "$1"
+            shift
+            ;;
+        0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9)
+            display_number "$1"
             shift
             ;;
         *) # no more options
